f8823f0cace0295a67a73f82272fe9905285dbf1
HP-CELANI\riccc <ricc.celani@gmail.com>
2024-03-08 17:12:04 +0100

    Aggiunto footer e miglioramento css/layout per login, header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from "./header-component/header-component.component";
 import { LoginComponent } from "./login/login.component";
+import { FooterComponentComponent } from "./footer-component/footer-component.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponentComponent]
+    imports: [RouterOutlet, HeaderComponentComponent, FooterComponentComponent]
 })
 export class AppComponent {
   title = 'GestArticoli';
