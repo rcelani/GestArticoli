5f69a6bf2179e14a5e83908eae2669d1414c9f61
HP-CELANI\riccc <ricc.celani@gmail.com>
2024-03-09 02:07:28 +0100

    implementazione del singolo paragrafo e aggiunto all'array di paragrafi dell'Articolo

--- src/app/admin-dashboard/create-article-form/create-article-form.component.ts
+++ src/app/admin-dashboard/create-article-form/create-article-form.component.ts
@@ -11,33 +11,67 @@ import { FormArray, FormBuilder, FormGroup, FormsModule, Validators, ReactiveFor
 })
 export class CreateArticleFormComponent {
   formArticles!: FormGroup;
-  languages: Array<string> = [
-    "Italiano",
-    "Inglese",
-    "Spagnolo",
-    "Francese"
+  arrLanguages: Array<string> = ["Italiano", "Inglese", "Spagnolo", "Francese"];
+  arrCapoluoghi: Array<string> = [
+    "L'Aquila", "Potenza", "Catanzaro", "Napoli",
+    "Bologna", "Trieste", "Roma", "Genova",
+    "Milano", "Ancona", "Campobasso", "Torino",
+    "Bari", "Cagliari", "Palermo", "Firenze",
+    "Trento", "Perugia", "Aosta", "Venezia"
   ];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     /* qui definisco e inizializzo il form group dell'articolo usando i validators */
-    this.formArticles = this.fb.group({
-      lingua: [this.languages[0], Validators.required],
+    this.formArticles = this.formBuilder.group({
+      lingua: [this.arrLanguages[0], Validators.required],
       title: ['', [Validators.required, Validators.maxLength(15)]],
       subtitle: ['', Validators.maxLength(17)],
       description: ['', Validators.required],
-      destinationID: ['', Validators.required],
+      cittaArticle: ['', Validators.required],
       mediaImage: [null, Validators.required], /* manca controllo max misure img */
-      paragraphs: this.fb.array([]) /* creo array di paragrafi */
+      paragraphs: this.formBuilder.array([]) /* creo array di paragrafi */
     });
   }
 
+  /* sto specificando la proprietà che voglio accedere del form e associarlo e specifico il tipo di ritorno */
+  get paragraphs() {
+    return this.formArticles.get('paragraphs') as FormArray;
+  }
+
+  /* metodo che crea un paragrafo nuovo e lo aggiunge all'array di paragrafi */
+  addParagraph() {
+    this.paragraphs.push(this.formBuilder.group({
+        paragraphTitle: ['', Validators.required],
+        paragraphText: ['', Validators.required],
+        cittaParagraph: ['', Validators.required],
+        mediaImage: [null, Validators.required]
+      })
+    );
+  }
+
+  /* submit del form */
   onSubmit() {
     if (this.formArticles.valid) {
       console.log(this.formArticles.value);
+      // implementare invio alle api che salverà il dato nel db
     } else {
       console.error('Form is invalid');
+      /* implementare messaggio di errore */
     }
   }
+
+  articlesImageChange(event: any) {
+    // implementare evento cambio immagine dell'Articolo 
+  }
+
+  paragraphImageChange(event: any, index: number) {
+    // implementare evento cambio immagine dei Paragrafi
+  }
 }
+
+
+
+
+
