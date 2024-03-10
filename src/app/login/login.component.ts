8eafc9f5b8099526ecb0acc556c3069678ffe3aa
HP-CELANI\riccc <ricc.celani@gmail.com>
2024-03-11 00:11:51 +0100

    risolto bug in credenziali login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,27 +21,27 @@ export class LoginComponent {
   constructor(
     private router: Router,
     private authService: AuthenticationService
-  ) {}
+  ) { }
 
   login() {
+    if (this.username === '' || this.password === '') {
+      this.error = true;
+      this.errorLogin = 'Credenziali non inserite';
+    }else {
       this.authService.getUserLogin(this.username, this.password).subscribe(user => {
-        if (user) {
-          if (user.role === 'admin') {
-            /* role user, verrà dirottato in dashboard Admin */
-            this.router.navigate(['/dashboard/admin'], { queryParams: { username: this.username }});
-          } else if (user.role === 'user') {
-            /* role user, verrà dirottato alla dashboard dello user */
-            this.router.navigate(['/dashboard/user'], { queryParams: { username: this.username }});
-          } 
-        } else {
-          if (this.username === '' || this.password === '') {
-            this.error = true;
-            this.errorLogin = 'Credenziali non inserite';
-          } else {
-            this.error = true;
-            this.errorLogin = 'Credenziali non valide';
-          }
+        if (this.username !== user?.username && this.password !== user?.password) {
+          this.error = true;
+          this.errorLogin = 'Credenziali non valide';
+        } else if (user.role === 'admin') {
+          /* role user, verrà dirottato in dashboard Admin */
+          this.error = false;
+          this.router.navigate(['/dashboard/admin'], { queryParams: { username: this.username } });
+        } else if (user.role === 'user') {
+          /* role user, verrà dirottato alla dashboard dello user */
+          this.error = false;
+          this.router.navigate(['/dashboard/user'], { queryParams: { username: this.username } });
         }
       });
+    }
   }
 }
